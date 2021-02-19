44be13afa61738effb311a1e3ecac701dd1716dc
floter1 <floter1@gmail.com>
2021-02-19 10:05:35 +0800

    auto push sh

--- pushgit.sh
+++ pushgit.sh
@@ -1,4 +1,4 @@
-git status
+git pull --commit https://floter1:596169b7129beff835abb1f360cff00afd7c00c0@github.com/floter1/root.git
 git add -A
 git commit -m "auto push sh"
-git push --repo https://floter1:General001*@github.com/floter1/root.git
+git push --repo https://floter1:596169b7129beff835abb1f360cff00afd7c00c0@github.com/floter1/root.git
